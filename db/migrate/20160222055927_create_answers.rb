fdc90c13cd4d0baa51c9df985324523888137eed
shamkeev <shamkeyev@mail.ru>
2016-02-22 12:37:08 +0600

    Added default value as false to an answer

--- db/migrate/20160222055927_create_answers.rb
+++ db/migrate/20160222055927_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :answer
-      t.boolean :correct
+      t.boolean :correct, default: false
 
       t.timestamps null: false
     end
