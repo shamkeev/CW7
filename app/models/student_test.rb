36e8288965d15fb772c4f205d1c68696d0c74faf
shamkeev <shamkeyev@mail.ru>
2016-02-22 18:18:50 +0600

    fixed bug

--- app/models/student_test.rb
+++ app/models/student_test.rb
@@ -2,5 +2,5 @@ class StudentTest < ActiveRecord::Base
   belongs_to :test
   validates :firstname, presence: true
   validates :lastname, presence: true
-  validate :group, presence: true
+  validates :group, presence: true
 end
