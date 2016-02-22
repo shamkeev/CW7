d2edfe301af0fa0072638f762253ba47ebc27249
shamkeev <shamkeyev@mail.ru>
2016-02-22 15:34:47 +0600

    Check for 1 correct answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,15 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   validates :answer, presence: true
+  validate :check_other_corrects
+
+  def check_other_corrects
+    answers = self.question.answers
+    if answers.count > 0
+      if self.correct and answers.where(correct: true).exists?
+        errors.add(:correct, 'there is already correct answer')
+      end
+    end
+  end
+
 end
